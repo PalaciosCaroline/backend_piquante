d9162ab51b70660549ba0f884fc0c16a54d40c72
PalaciosCaroline <caroline2807@yahoo.fr>
2023-06-01 14:51:14 +0200

    correction sur le schema

--- models/sauce.js
+++ models/sauce.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const SauceSchema = mongoose.Schema({
   name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: {type: String, required: false},
+  mainPepper: {type: String, required: false},
   imageUrl: { type: String, required: true },
+  heat: { type: Number, required: false, min: 1, max: 10 },
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   usersLiked: { type: [String], default: [] },
